64234637e404f6e4147f406580ade309d1e8ab38
Nedim Kulovac <nedim.kulovac@gmail.com>
2021-05-01 01:00:31 +0200

    question app created and question and answer models moved to this app (from quiz app). question model modified to suit its purposes

--- core/settings.py
+++ core/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quiz',
     'rest_framework',
+    'question',
+    'quiz',
     'score',
 ]
 
